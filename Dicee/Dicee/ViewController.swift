5e8c87d2a1debc7c6d5687f75187d2b2e5e3b05f
joneshshrestha <contact@joneshshrestha.com>
2019-09-06 18:11:46 +0545

    update

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // This will get executed when the roll button gets pressed.
+    }
+    
 }
 
