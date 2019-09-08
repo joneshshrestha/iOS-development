1b4bd2f54a84cd327a14c437354fbaf8c12ec9dd
joneshshrestha <contact@joneshshrestha.com>
2019-09-08 11:32:15 +0545

    update

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -19,18 +19,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateDiceImages()
+
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        updateDiceImages()
+        
+    }
+    
+    func updateDiceImages() {
+        
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
         
-        
     }
-    
 }
 
