13f000cf3ec611846a2a524c278ac2f82fc42f8c
joneshshrestha <contact@joneshshrestha.com>
2019-10-03 13:52:12 +0545

    update quizzler

--- Quizzler/Quizzler/Question.swift
+++ Quizzler/Quizzler/Question.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+class Question {
+    let questionText: String
+    let answer: Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+}
