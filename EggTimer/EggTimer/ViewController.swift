ea7f4ab7b38ec293214c5fd0948c3b6f4e0ae20b
joneshshrestha <contact@joneshshrestha.com>
2020-02-19 22:45:30 +0545

    add time

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+        if hardness == "Soft" {
+            print(softTime)
+        } else if hardness == "Medium" {
+            print(mediumTime)
+        } else {
+            print(hardTime)
+        }
+    }
+    
 
 }
