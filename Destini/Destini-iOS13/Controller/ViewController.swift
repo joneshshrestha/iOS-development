153753358aac94a90a5b5d4cca4ed5031dc3b5b4
joneshshrestha <contact@joneshshrestha.com>
2020-03-12 12:54:01 +0545

    updat viewDidLoad()

--- Destini/Destini-iOS13/Controller/ViewController.swift
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -6,14 +6,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = story0
     }
+    
+    let story0 = "You see a fork in the road."
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."
 
 
 }
