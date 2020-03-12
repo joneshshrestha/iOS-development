a9ef213a88c7bb7a41d4f8c60ecd21813c264909
joneshshrestha <contact@joneshshrestha.com>
2020-03-12 12:43:24 +0545

    update

--- Destini/Destini-iOS13/Controller/ViewController.swift
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -1,10 +1,6 @@
 //
 //  ViewController.swift
 //  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import UIKit
 
--- Destini/Destini-iOS13/Model/Story.swift
+++ Destini/Destini-iOS13/Model/Story.swift
@@ -1,9 +1,5 @@
 //
 //  Sotry.swift
 //  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import Foundation
