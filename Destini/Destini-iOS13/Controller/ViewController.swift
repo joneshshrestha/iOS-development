21d504e3b2b6cc5412a74b2eee525aaa80111421
joneshshrestha <contact@joneshshrestha.com>
2020-03-13 07:32:07 +0545

    update README

--- Destini/Destini-iOS13/Controller/ViewController.swift
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -5,7 +5,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBAction func choiceMade(_ sender: UIButton) {
     }
@@ -16,12 +16,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         storyLabel.text = story0
+        
+        choice1Button.setTitle("\(choice1)",for: .normal)
+        choice2Button.setTitle("\(choice2)", for: .normal)
     }
     
     let story0 = "You see a fork in the road."
     let choice1 = "Take a left."
     let choice2 = "Take a right."
-
-
+    
 }
 
