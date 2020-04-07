2269db7acb6e6419037d55a4a7cc1a82b0c45030
joneshshrestha <contact@joneshshrestha.com>
2020-04-07 21:15:36 +0545

    new project

---
+++ Birthday/Birthday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Birthday
+//
+//  Created by Jonesh Shrestha on 4/7/20.
+//  Copyright © 2020 Jonesh Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
