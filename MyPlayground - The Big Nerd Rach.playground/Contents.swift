bc76dd8531cd4a61712a738b533abdbc5ddaf34d
joneshshrestha <contact@joneshshrestha.com>
2020-05-14 10:26:39 +0545

    update

--- MyPlayground - The Big Nerd Rach.playground/Contents.swift
+++ MyPlayground - The Big Nerd Rach.playground/Contents.swift
@@ -1,3 +1,16 @@
 import Cocoa
 
-var str = "Hello, playground"
+var numberOfStopLights: Int = 4
+var population: Int = 10
+population = 30
+let townName: String = "Narayangarh"
+var unemployment: Int = 10
+let townDescription = "\(townName) has a population of \(population) with \(unemployment) unemployed and \(numberOfStopLights) stoplights"
+
+population = 10000
+if population < 10000 {
+    print("\(townName) is a small city")
+} else if population >= 10000 {
+    print("\(townName) is pretty big!")
+}
+
