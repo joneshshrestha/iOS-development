669db0ce04a6e883679fae0a22ee4deb6a380b88
joneshshrestha <contact@joneshshrestha.com>
2020-06-13 08:09:57 +0545

    update Destini

--- Destini/Destini-iOS13/Controller/ViewController.swift
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -1,29 +1,24 @@
 //
 //  ViewController.swift
 //  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
 
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
-    @IBAction func choiceMade(_ sender: UIButton) {
-    }
+
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = story0
-        
-        choice1Button.setTitle("\(choice1)",for: .normal)
-        choice2Button.setTitle("\(choice2)", for: .normal)
+
     }
-    
-    let story0 = "You see a fork in the road."
-    let choice1 = "Take a left."
-    let choice2 = "Take a right."
-    
+
+
 }
 
